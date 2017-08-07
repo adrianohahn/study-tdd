8516b4eff295c2f90628cea09a4007fad8220102
adrianohahn <adrianohahn@gmail.com>
2017-08-07 20:29:56 -0300

    Invoque o método test (TestCase)

--- study-tdd-xUnit/test.py
+++ study-tdd-xUnit/test.py
@@ -1,6 +1,8 @@
 from xunit import *
 
-test = WasRun("testMethod")
-print test.wasRun
-test.run()
-print test.wasRun
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
