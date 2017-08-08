9eace8695ba6a782b55f22057b6becb58bf3ecbd
adrianohahn <adrianohahn@gmail.com>
2017-08-07 21:51:17 -0300

    Display test failure

--- study-tdd-xUnit/xunit.py
+++ study-tdd-xUnit/xunit.py
@@ -2,8 +2,12 @@ class TestResult:
 
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def testFailed(self):
         self.runCount = self.runCount + 1
 
     def summary(self):
@@ -17,7 +21,10 @@ class TestCase:
         result = TestResult()
         result.testStarted()
         self.setUp()
-        method = getattr(self,self.name)
-        method()
+        try:
+            method = getattr(self,self.name)
+            method()
+        except:
+            result.testFailed()
         self.tearDown()
         return result
