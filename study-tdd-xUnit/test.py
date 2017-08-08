971fd0b668fffdec3268b63fe83666462ce432c3
adrianohahn <adrianohahn@gmail.com>
2017-08-07 21:09:42 -0300

    Invoque o método test (correções)

--- study-tdd-xUnit/test.py
+++ study-tdd-xUnit/test.py
@@ -1,8 +1,25 @@
 from xunit import *
 
+class WasRun(TestCase):
+    def __init__(self,name):
+        TestCase.__init__(self,name)
+        self.wasRun = None
+    pass
+
+    def testMethod(self):
+        self.wasRun = 1
+
 class TestCaseTest(TestCase):
+
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert test.wasSetUp
+
     def testRunning(self):
         test = WasRun("testMethod")
         assert(not test.wasRun)
         test.run()
         assert(test.wasRun)
+
+TestCaseTest("testRunning").run()
--- study-tdd-xUnit/xunit.py
+++ study-tdd-xUnit/xunit.py
@@ -2,14 +2,15 @@ class TestCase:
     def __init__(self,name):
         self.name = name
 
+    def run(self):
+        method = getattr(self,self.name)
+        method()
+
 class WasRun(TestCase):
     def __init__(self,name):
         TestCase.__init__(self,name)
         self.wasRun = None
     pass
 
-    def run(self):
-        method = getattr(self,self.name)
-        method()
     def testMethod(self):
         self.wasRun = 1
