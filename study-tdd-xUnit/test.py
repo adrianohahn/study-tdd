04ebd469c3092e17b0c423bf357f46afa81d74bf
adrianohahn <adrianohahn@gmail.com>
2017-08-07 21:27:23 -0300

    String de registro em WasRun

--- study-tdd-xUnit/test.py
+++ study-tdd-xUnit/test.py
@@ -6,23 +6,20 @@ class WasRun(TestCase):
         self.wasRun = None
 
     def setUp(self):
-        self.wasSetUp = True
+        self.log = "setUp "
 
     def testMethod(self):
         self.wasRun = True
+        self.log = self.log + "testMethod "
 
 class TestCaseTest(TestCase):
 
     def setUp(self):
         self.test = WasRun("testMethod")
 
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
-
     def testRunning(self):
-        assert(not self.test.wasRun)
         self.test.run()
-        assert(self.test.wasRun)
+        assert("setUp testMethod " == self.test.log)
+
 
 TestCaseTest("testRunning").run()
